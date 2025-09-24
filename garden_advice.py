56aebdcbe272a6c3f10e9f55367be1466176bbcf
Thamsanqa <zimele.mngoma@gmail.com>
2025-09-24 17:26:22 +0200

    Add TODOs to garden_advice.py for future improvements

--- garden_advice.py
+++ garden_advice.py
@@ -1,1 +1,12 @@
+
+"""
+garden_advice.py
+A simple script that gives gardening tips.
+"""
+
+# TODO: Refactor into a function called get_garden_advice() that returns advice instead of just printing
+# TODO: Replace hardcoded advice with a dictionary that gives tips based on season/month
+# TODO: Add simple unit tests for get_garden_advice()
+
+# Current basic version
 print("Remember to water your plants daily!")
